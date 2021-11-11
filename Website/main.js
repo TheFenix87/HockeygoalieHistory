ffb14505bf866e79c6c54dcd7f026cec84d4e75d
TheFenix87 <bzurita34@gmail.com>
2021-11-11 15:04:09 -0500

    This is the whole site lol

---
+++ Website/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+  $("#button1").click(function(){
+    $("#m21").fadeToggle();
+  });
+});
+$(document).ready(function(){
+  $("#button2").click(function(){
+    $("#m22").fadeToggle();
+  });
+});
+$(document).ready(function(){
+  $("#button3").click(function(){
+    $("#m23").fadeToggle();
+  });
+});
+$(document).ready(function(){
+  $("#button4").click(function(){
+    $("#m24").fadeToggle();
+  });
+});
+$(document).ready(function(){
+  $("#button5").click(function(){
+    $("#m25").fadeToggle();
+  });
+});
+$(document).ready(function(){
+  $("#button6").click(function(){
+    $("#m26").fadeToggle();
+  });
+});
+mediumZoom ('.six',{
+	template: '#six-template',
+	container: '#six-container',
+	scrollOffset:600
+})
+mediumZoom ('.seven',{
+	template: '#seven-template',
+	container: '#seven-container',
+	scrollOffset:600
+})
+mediumZoom ('.eight',{
+	template: '#eight-template',
+	container: '#eight-container',
+	scrollOffset:600
+})
+mediumZoom ('.nine',{
+	template: '#nine-template',
+	container: '#nine-container',
+	scrollOffset:600 
+})
+mediumZoom ('.zero',{
+	template: '#zero-template',
+	container: '#zero-container',
+	scrollOffset:600
+})
+mediumZoom ('.ten',{
+	template: '#ten-template',
+	container: '#ten-container',
+	scrollOffset:600
+})
+
